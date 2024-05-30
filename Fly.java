1d9cafe90ce12381a4eb741ee368b6a2569f54cd
Benoit-Hayet <hayetbenoit@gmail.com>
2024-05-30 14:27:16 +0200

    Ok

--- Fly.java
+++ Fly.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Fly {
+public interface Fly {
+
+    void takeOff();
+
+    int ascend(int meters);
+
+    int descend(int meters);
+
+    void glide();
+
+    void land();
 }
